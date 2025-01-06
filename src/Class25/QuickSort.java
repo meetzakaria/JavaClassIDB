2cbb942cd9b793a31b929da6f6487a7d76c8ccf4
Java Student PC-9 <zakariahossen65@gmail.com>
2025-01-06 11:57:40 +0600

    IDB Class Changes

---
+++ src/Class25/QuickSort.java
@@ -0,0 +1,45 @@
+package Class25;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {38, 27, 43, 3, 9, 82, 10};
+
+        System.out.println("Original array: " + Arrays.toString(array));
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
